8eeca3a500f3137f9aa2a5d282cd6c8b1fe9d700
zhang <zhang@prismed.jp>
2017-08-26 09:01:01 +0900

    WhiteNoise & Connect

---
+++ MDtestwinform/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MDtestwinform
+{
+    static class Program
+    {
+        /// <summary>
+        /// アプリケーションのメイン エントリ ポイントです。
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Console.WriteLine("MD test...");
+
+
+            //Render the form
+            var formWindow = new Form1();
+            formWindow.Show();
+            Application.Run();
+        }
+    }
+ 
+
+}
